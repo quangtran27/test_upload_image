75f982206e60b567c5eb75e1bf9a69ad7de9fe9d
Quang Tran <quangtv2702@gmail.com>
2023-06-02 05:43:32 +0700

    update

--- test_upload_image/settings.py
+++ test_upload_image/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'tmp/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
